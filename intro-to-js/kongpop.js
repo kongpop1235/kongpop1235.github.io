86b1c30013c8b7632a2e19505510f6b755007c97
Kognpop Ruschatapaparpong <ru.kongpop@hotmail.com>
2022-02-18 14:04:19 +0700

    update

--- intro-to-js/kongpop.js
+++ intro-to-js/kongpop.js
@@ -11,3 +11,16 @@ function helloWorld() {
   const h1message = document.getElementById("name-h1").innerHTML
   alert(h1message + " : ทุกปัญหามีทางแก้ ทุกเรื่องแย่มีทางออก"); // ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
 }
+
+function calAge() {
+  const BirthYear = prompt("Enter your birth year (AD)", "");
+  const age = new Date().getFullYear() - BirthYear;
+  const message1 = "ยินดีด้วยคุณอยู่ในประเทศนี้มา";
+  const message2 = "ปีแล้ว";
+  alert(`${message1} ${age} ${message2}`);
+}
+
+function loadProfile() {
+  document.getElementById("my-name").innerHTML = prompt("Enter your name", "");
+  document.getElementById("my-province").innerHTML = prompt("Enter province", "");
+}
